e4acee94b9941b2a668f8b3a930376f9ecfe3fe2
Adanna Omatu <adaomatu@gmail.com>
2017-12-14 17:08:36 -0500

    completed user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,16 @@
 class UsersController < ApiController
   before_action :require_login, except: [:create]
-
+  # create! returns the actual created user and create returns true
   def create
     user = User.create!(user_params)
     if user.save
       render json: { token: user.auth_token }
     else
       render json: user.errors
+    end
   end
 
-  # this method gives info about current user info  instead of user/id
+  # this method gives info about current user using the user/id path
   def profile
     user = User.find_by_auth_token!(request.headers[:token])
     render json: { user: { userame: user.username, email: user.email } }

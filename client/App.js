300cbca68691f0f8d72d3366b10715005b735d4f
Adanna Omatu <adaomatu@gmail.com>
2017-12-14 14:57:09 -0500

    fetch not working

--- client/App.js
+++ client/App.js
@@ -1,15 +1,41 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { StyleSheet, Text, View, Button } from 'react-native'
 
 export default class App extends React.Component {
-  render() {
+  constructor (props) {
+    super(props)
+    this.state = {
+      isLoading: true
+    }
+  }
+
+  async fetchShow () {
+    try {
+      let headers = {
+        'Content-Type': 'application/json',
+        'trakt-api-version': '2',
+        'trakt-api-key': 'dc60acabe3d5d953393a0e332d8d7d3c5e3c7b7710158b5223526e79f1cf597a'
+      }
+      let response = await fetch(
+        'https://api-staging.trakt.tv/shows/game-of-thrones?extended=full', headers
+      )
+      let responseJson = await response.json()
+      console.log(responseJson.show)
+      return responseJson.show
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  render () {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        {/* <Text>Open App.js to start working on your app!</Text> */}
         <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Text >Shake your phone to open the developer menu.</Text>
+        <Button>onPress={this.fetchShow.bing(this) }</Button>
       </View>
-    );
+    )
   }
 }
 
@@ -18,6 +44,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    justifyContent: 'center'
+  }
+})

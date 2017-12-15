5a0c883d810868d03f9904fb0f46482f6054adbf
Adanna Omatu <adaomatu@gmail.com>
2017-12-14 23:17:19 -0500

    able to query api for user specific shows

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -2,14 +2,22 @@ class ShowsController < ApiController
   before_action :require_login
 
   def index
-    shows = Show.all.includes(:user)
-    render json: { shows: shows.map(&:serialize) }
+    shows = Show.where(user_id: current_user.id)
+    if shows.blank?
+      render json: { message: 'No shows added' }
+    else
+      render json: { shows: shows }
+    end
   end
 
+  # api can only give results of a show to the current user, if querry doesn't math it will throw error message
   def show
-    show = show.find(params[:id])
-    show_user = Show.user
-    render json: { show: show, user: show_user.username }
+    show = Show.where('user_id = ? AND id = ?', current_user.id, params[:id])
+    if show.blank?
+      render json: { message: 'No show with that Id' }
+    else
+      render json: { show: show }
+    end
   end
 
   def create
@@ -18,22 +26,23 @@ class ShowsController < ApiController
     if show.save
       render json: {
         message: 'ok',
-        show: show,
+        show: show
       }
     else
-      render json: {message: 'Could not add show to the watchlist'}
+      render json: { message: 'Could not add show to the watchlist' }
     end
   end
 
   def destroy
-    show = show.find(params[:id])
+    show = Show.find(params[:id])
     show.destroy!
-    render json: {message: "#{show.name} deleted"}
+    render json: { message: "#{show.name} deleted" }
   end
 
   private
+
   def show_params
-    params.require(:show).permit(:name, :trakt_id, :tvdb_id,:image_url)
+    params.require(:show).permit(:name, :trakt_id, :tvdb_id, :image_url)
   end
 
 end

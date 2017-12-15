7ab4ac63c7495cbf1d27608da0dc35b30049dc4d
Adanna Omatu <adaomatu@gmail.com>
2017-12-14 21:55:44 -0500

    added index, show, and changed destroy methods in show controller

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -1,13 +1,16 @@
 class ShowsController < ApiController
   before_action :require_login
 
-  # def index
-  #   shows = Show.all
-  #   render json: { shows: shows }
-  # end
+  def index
+    shows = Show.all.includes(:user)
+    render json: { shows: shows.map(&:serialize) }
+  end
 
-  # def show
-  #   show = Show.find
+  def show
+    show = show.find(params[:id])
+    show_user = Show.user
+    render json: { show: show, user: show_user.username }
+  end
 
   def create
     show = Show.new(show_params)
@@ -23,16 +26,14 @@ class ShowsController < ApiController
   end
 
   def destroy
+    show = show.find(params[:id])
     show.destroy!
     render json: {message: "#{show.name} deleted"}
   end
-  
+
   private
   def show_params
     params.require(:show).permit(:name, :trakt_id, :tvdb_id,:image_url)
   end
-
-end
-
 
 end
